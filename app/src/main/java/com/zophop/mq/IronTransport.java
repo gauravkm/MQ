eb8a1dba2293e32ba54e7738a0b9b7fef40cbd10
Gaurav Mittal <gaurav.mi@directi.com>
2015-06-18 14:12:13 +0530

    Add configuration as constructor params in IronTransport

--- app/src/main/java/com/zophop/mq/IronTransport.java
+++ app/src/main/java/com/zophop/mq/IronTransport.java
@@ -3,6 +3,7 @@ package com.zophop.mq;
 import java.io.IOException;
 
 import io.iron.ironmq.Client;
+import io.iron.ironmq.Cloud;
 import io.iron.ironmq.Queue;
 
 public class IronTransport implements Transport {
@@ -10,8 +11,8 @@ public class IronTransport implements Transport {
     private Client _client;
     private Queue _queue;
 
-    public IronTransport(String queueName) {
-        _client = new Client();
+    public IronTransport(String projectId, String token, Cloud cloud, Integer apiVersion, String queueName) {
+        _client = new Client(projectId, token, cloud, apiVersion);
         _queue = _client.queue(queueName);
     }
 
